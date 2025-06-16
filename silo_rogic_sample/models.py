00d8900b6904cc917d8f5427044bd62cf84e6b4c
Takushi Sato <n-ts-sato@sakura-communication.co.jp>
2025-06-16 13:53:08 +0900

    比較情報モデルを追加

--- silo_rogic_sample/models.py
+++ silo_rogic_sample/models.py
@@ -10,6 +10,7 @@ class SiloMaster(models.Model):
     inbound_max_weight = models.IntegerField("最大入庫量")
     max_weight = models.IntegerField("最大容量")
     should_calculate = models.BooleanField("計算対象", default=False)
+    should_compare_with_others = models.BooleanField("他サイロとの比較対象", default=False)
 
     def __str__(self):
         return self.silo_name
@@ -20,15 +21,19 @@ class SiloMaster(models.Model):
         verbose_name_plural = "サイロマスタ"
         db_table = "silo_master"
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    def clean(self):
         if self.should_calculate and not hasattr(self, "formula"):
             raise ValidationError("should_calculate=True の場合、SiloFormula を作成してください。")
+        if self.should_compare_with_others and not self.compare_with_silo.exists():
+            raise ValidationError("should_compare_with_others=True の場合、SiloCompare を作成してください。")
+
 
 
 class SiloFormula(models.Model):
     """
     SiloMasterに対応した計算式の情報（should_calculate=Trueの場合のみ存在）
+
+    サイロに対して何か計算式がある場合、こちらに登録する
     """
     silo = models.OneToOneField(SiloMaster, on_delete=models.CASCADE, related_name="formula")
     formula_text = models.TextField("計算式の内容", blank=True, null=True)
@@ -41,3 +46,22 @@ class SiloFormula(models.Model):
         verbose_name = "サイロ計算式"
         verbose_name_plural = "サイロ計算式"
         db_table = "silo_formula"
+
+
+class SiloCompare(models.Model):
+    """
+    他サイロとの比較対象の情報
+
+    比較するサイロがある場合、こちらに登録しておく
+    """
+    source_silo = models.ForeignKey(SiloMaster, verbose_name="比較元サイロ", on_delete=models.CASCADE, related_name="comparisons_as_source")
+    target_silo = models.ForeignKey(SiloMaster, verbose_name="比較先サイロ", on_delete=models.CASCADE, related_name="comparisons_as_target")
+
+
+    def __str__(self):
+        return f"Compare {self.silo.silo_name} with {self.compare_with_silo.silo_name}"
+
+    class Meta:
+        verbose_name = "サイロ比較"
+        verbose_name_plural = "サイロ比較"
+        db_table = "silo_compare"
