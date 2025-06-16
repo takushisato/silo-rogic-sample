6762702e08a6f7f3871e7a3ee40eb4ea8c4792d4
Takushi Sato <n-ts-sato@sakura-communication.co.jp>
2025-06-16 10:43:09 +0900

    サイロのテーブル追加

--- silo_rogic_sample/models.py
+++ silo_rogic_sample/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class SiloMaster(models.Model):
+    """
+    should_calculate: 計算対象になるかを判定するフラグ
+    """
+    silo_name = models.CharField("サイロNo", max_length=50)
+    disabled = models.BooleanField("直接入庫対象外", default=False)
+    inbound_max_weight = models.IntegerField("最大入庫量")
+    max_weight = models.IntegerField("最大容量")
+    should_calculate = models.BooleanField("計算対象", default=False)
+
+    def __str__(self):
+        return self.silo_name
+
+    class Meta:
+        ordering = ["silo_name"]
+        verbose_name = "サイロマスタ"
+        verbose_name_plural = "サイロマスタ"
+        db_table = "silo_master"
